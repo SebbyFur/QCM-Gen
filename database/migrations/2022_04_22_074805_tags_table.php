5f789d9e48fb4d5e0f906a8700c3c91f795d952f
diot0022 <sebastien.diot@etudiant.univ-reims.fr>
2022-04-25 10:50:04 +0200

    First commit

---
+++ database/migrations/2022_04_22_074805_tags_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Tags;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag');
+        });
+
+        Tags::insert(['tag' => 'C']);
+        Tags::insert(['tag' => 'C++']);
+        Tags::insert(['tag' => 'Go']);
+        Tags::insert(['tag' => 'Python']);
+        Tags::insert(['tag' => 'Laravel']);
+        Tags::insert(['tag' => 'Javascript']);
+        Tags::insert(['tag' => 'HTML']);
+        Tags::insert(['tag' => 'CSS']);
+        Tags::insert(['tag' => 'Typescript']);
+        Tags::insert(['tag' => 'Lua']);
+        Tags::insert(['tag' => 'Java']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
